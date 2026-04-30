7328c57270e510d7c07fed5222c004025325a522
Emma Heikkinen <emhe2504@student.miun.se>
2026-04-30 12:43:57 +0200

    Startade upp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Courses } from './courses/courses';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "Courses", component: Courses},
+    { path: "", redirectTo: "Courses", pathMatch: "full"}
+];
