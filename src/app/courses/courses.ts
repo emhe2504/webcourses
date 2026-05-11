4257a6368ef4115a49fab12f9c0c5992b35c3273
Emma Heikkinen <emhe2504@student.miun.se>
2026-05-11 14:47:27 +0200

    kursfilter

--- src/app/courses/courses.ts
+++ src/app/courses/courses.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { course } from '../models/courseInt';
 import { CoursesService } from '../services/getcourses';
 
@@ -10,8 +10,22 @@ import { CoursesService } from '../services/getcourses';
 })
 export class CoursesComponent {
 
-  courses = signal<course[]>([]);   //signal för hantera courses, startvärde tom []
-  error = signal<string | null>(null);    //signal för att hantera error, startvärde null
+  courses = signal<course[]>([]);   //signal som lagrar courses, startvärde tom array
+  error = signal<string | null>(null);    //signal som lagrar error, startvärde null
+
+  searchPhrase = signal("");  //signal som lagrar sökfras
+
+  //signal som lagrar uträknat (filtrerat) värde
+  filterCourses = computed(() => {
+    const searchFilter = this.searchPhrase().trim().toLowerCase();  //Sökfrasen vi matade in (inga mellanstag, gemener)
+    if (!searchFilter) return this.courses(); //Ingen sökfras, visa alla kurser
+
+    return this.courses().filter(course =>
+      course.code.toLowerCase().includes(searchFilter) || //returnera kurskoder som inkluderar sökfras
+      course.coursename.toLowerCase().includes(searchFilter) || //returnera kursnamn som inkluderar sökfras
+      course.progression.toLowerCase().includes(searchFilter) //returnera progression som inkluderar sökfras
+    )
+  });
 
   getcourses = inject(CoursesService);
 
